74e50749d7e258d57bc56292dac04bb66612aaed
oripwk <ori.popowski@tg-17.com>
2018-04-01 09:21:24 +0300

    Change mocking to Mockk

--- src/test/kotlin/com/oripwk/controllers/UserControllerSpec.kt
+++ src/test/kotlin/com/oripwk/controllers/UserControllerSpec.kt
@@ -1,7 +1,5 @@
 package com.oripwk.controllers
 
-import com.nhaarman.mockito_kotlin.mock
-import com.nhaarman.mockito_kotlin.whenever
 import com.oripwk.client.AlbumClient
 import com.oripwk.model.User
 import com.oripwk.service.UserService
@@ -11,22 +9,19 @@ import kotlinx.coroutines.experimental.runBlocking
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.it
 import org.jetbrains.spek.api.dsl.on
-import org.mockito.Mockito
+import io.mockk.*
 
 object UserControllerSpec : Spek({
     on("getting first album") {
         it("should pass") {
-            // TODO fix
-            runBlocking {
-                val userService: UserService = mock()
-                val albumClient: AlbumClient = mock()
-                val config: JsonObject = mock()
-                val ctx: RoutingContext = mock()
-                whenever(userService.getAll()).thenReturn(listOf(User(1, "name", "email", "album")))
-                val controller = UserController(userService, albumClient, config)
-                controller.getAll(ctx)
-                Mockito.verify(ctx)
-            }
+            val userService: UserService = mockk()
+            val albumClient: AlbumClient = mockk()
+            val config: JsonObject = mockk(relaxed = true)
+            val ctx: RoutingContext = mockk(relaxed = true)
+            coEvery { userService.getAll() } returns listOf(User(1, "name", "email", "album"))
+            val controller = UserController(userService, albumClient, config)
+            runBlocking { controller.getAll(ctx) }
+            verify { ctx.response() }
         }
     }
 })
